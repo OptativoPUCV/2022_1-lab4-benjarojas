1ec7d6c21903269f50dfcac6dedd9764908dd10f
benjarojas <benjathaboi@gmail.com>
2022-04-09 22:47:53 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,7 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
     long valorHash = hash(key, map->capacity);
-    long k = valorHash;
+    long k = valorHash + 1;
     //Pair* insertPair = createPair(key, value);
 
     if(valorHash >= map->size)
